e7b07a029291d5ef0e42fc72fcbf5721424fad19
E Mincarelli <sancho.poncho@laposte.net>
2021-08-17 10:30:39 +0100

    Tests of GrandPy Bot app

--- ChatBot/parser_question.py
+++ ChatBot/parser_question.py
@@ -69,7 +69,9 @@ class Parser:
         """
         keywords = []
         # read all words of file 'stopwords.txt'...
-        with open('C:/Users/utilisateur/Desktop/Formation_OpenClassRoom/Projet_7/PROJET/GrandPy_Bot/ChatBot/static/stopwords.txt', "r") as stopwords:
+        # with open('C:/Users/utilisateur/Desktop/Formation_OpenClassRoom/Projet_7/PROJET/GrandPy_Bot/ChatBot/static/stopwords.txt', "r") as stopwords:
+        with open('ChatBot/static/stopwords.txt', "r") as stopwords:
+
             lignes = stopwords.readlines()
             # transform the file 'stopwords.txt' into a list of words
             crible = lignes[0].split(',')
@@ -105,7 +107,7 @@ class Parser:
         keywords = []
         for word in list_of_words:
             # search the good file of verbs to see if the 'word' is a verb
-            file_path = 'C:/Users/utilisateur/Desktop/Formation_OpenClassRoom/Projet_7/PROJET/GrandPy_Bot/ChatBot/static/Verbes_en_francais/' + cls.verbs_file_name(word)
+            file_path = 'ChatBot/static/Verbes_en_francais/' + cls.verbs_file_name(word)
             with open(file_path, "r") as stop_verbs:
                 lignes = stop_verbs.readlines()
                 crible = lignes[0].split(',')
