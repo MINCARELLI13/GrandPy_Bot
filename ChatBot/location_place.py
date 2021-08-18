5756b36b9a028e40a5fa517dfd363bac5ef7580f
E Mincarelli <sancho.poncho@laposte.net>
2021-08-18 14:30:33 +0100

    cleaning of code

--- ChatBot/location_place.py
+++ ChatBot/location_place.py
@@ -6,7 +6,8 @@
 # coding: utf-8
 
 import requests
-from os import environ
+import os
+from boto.s3.connection import S3Connection
 
 
 class Place:
@@ -25,7 +26,7 @@ class Place:
         PARAMS = {"input": spot,
                   "inputtype": "textquery",
                   "fields": "formatted_address,geometry,name",
-                  "key": environ.get('Google_Place_KEY')}
+                  "key": S3Connection(os.environ['Google_Place_KEY']}
         # "key": "AIzaSyAaICGdTIFPs_4qaw3g6FvdM5Gh2ZnoU9M"
         # sent the query to the API 'Google Place'
         response = requests.get(url=cls.URL, params=PARAMS)
